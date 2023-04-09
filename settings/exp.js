10bca013ae72783f2b89b174f24d12ed53d67a4a
Cohman Teodora <61310613+irisetto@users.noreply.github.com>
2023-04-09 19:53:42 +0300

    Delete settings directory

--- settings/exp.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-  const exportToggle = document.getElementById("export-toggle");
-  const exportPanel = document.getElementById("export-panel");
-
-  exportToggle.addEventListener("click", () => {
-    exportPanel.classList.toggle("hidden");
-  });
-});

6d938f0c0f8a9c62e1b3c4f921094cb94d832015
dand <dandodun@gmail.com>
2023-06-12 19:56:34 +0300

    create db pool

--- back-end/main.js
+++ back-end/main.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const pool = require("./util/db");
 
 const server = http.createServer((req, res) => {
   const url = req.url.split("?")[0];

7845906119db0e1b33c41bb0c212ac3a97f18af3
dand <dandodun@gmail.com>
2023-06-14 12:23:12 +0300

    auth

--- back-end/controller/apiController.js
+++ back-end/controller/apiController.js
@@ -6,23 +6,46 @@ const { handleSendCode } = require("./forgotPass");
 const { handleInsertCode } = require("./forgotPass");
 const authenticateJWT = require("../utils/authenticateJWT");
 
-
-
 exports.handleApiRequest = (req, res) => {
-  if (req.url.startsWith("/api/animals")) {
-    authenticateJWT(req, res, () => {
-      animalsController(req, res);
-    });
-  } else if (req.url.startsWith("/api/login")) {
-    handleLogin(req, res);
-  } else if (req.url.startsWith("/api/register")) {
-    handleRegister(req, res);
-  } else if (req.url.startsWith("/api/help")) {
-    handleHelp(req, res);
-  } else if (req.url.startsWith("/api/code")) {
-    handleSendCode(req, res);
-  }
-  else if (req.url.startsWith("/api/insertCode")) {
-    handleInsertCode(req, res);
+  if (req.method === "POST") {
+    if (req.url.startsWith("/api/login")) {
+      handleLogin(req, res);
+    } else if (req.url.startsWith("/api/register")) {
+      handleRegister(req, res);
+    } else if (req.url.startsWith("/api/insertCode")) {
+      handleInsertCode(req, res);
+    } else if (req.url.startsWith("/api/code")) {
+      handleSendCode(req, res);
+    } else {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("Not Found");
+    }
+  } else if (req.method === "GET") {
+    if (req.url.startsWith("/api/animals")) {
+      authenticateJWT(req, res, () => {
+        animalsController(req, res);
+      });
+    } else if (req.url.startsWith("/api/help")) {
+      authenticateJWT(req, res, () => {
+        handleHelp(req, res);
+      });
+    } else {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("Not Found");
+    }
+  } else if (req.method === "DELETE") {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Not Found");
+  } else if (req.method === "PUT") {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Not Found");
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Not Found");
   }
 };
--- back-end/controller/mainController.js
+++ back-end/controller/mainController.js
@@ -1,12 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 const url = require("url");
-const { handleRegister } = require("./register");
-const { handleLogin } = require("./login");
-const animalsController = require("./animalsController");
-const { handleHelp } = require("./helpController");
-const { handleSendCode } = require("./forgotPass");
-const { handleInsertCode } = require("./forgotPass");
 const handleStaticRequest = require("./staticController");
 const { handleApiRequest } = require("./apiController");
 
@@ -17,26 +11,5 @@ const handleRequest = (req, res) => {
     handleStaticRequest(req, res);
   }
 };
-
-const handlePostRequests = (req, res) => {
-  if (req.url.startsWith("/register")) {
-    handleRegister(req, res);
-  } else if (req.url.startsWith("/login")) {
-    handleLogin(req, res);
-  } else if (req.url.startsWith("/help")) {
-    handleHelp(req, res);
-  }
-   else if (req.url.startsWith("/forgotPassword")) {
-      handleSendCode(req, res);
-    }
-      else if (req.url.startsWith("/insertCode")) {
-        handleInsertCode(req, res);
-  } else res.end("post?");
-};
-
-// const handleApiRequest = (req, res) => {
-//   animalsController(req, res);
-//   //res.end("salut");
-// };
 
 module.exports = handleRequest;

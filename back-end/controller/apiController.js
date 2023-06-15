8392b381f4dc216c03a60df4d9fd995d2a8b5513
dand <dandodun@gmail.com>
2023-06-15 13:32:30 +0300

    i/e

--- back-end/controller/apiController.js
+++ back-end/controller/apiController.js
@@ -56,6 +56,10 @@ exports.handleApiRequest = (req, res) => {
         authenticateJWT(req, res, () => {
           usersController(req, res);
         });
+      } else if (req.url === "/api/animalNames") {
+        authenticateJWT(req, res, () => {
+          animalsController(req, res);
+        });
       }
       else {
         res.statusCode = 404;

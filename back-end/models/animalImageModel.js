b80e8ef9a1e7e83016da3f5a5cbb08f3680ff238
dand <dandodun@gmail.com>
2023-06-16 11:03:55 +0300

    animal image

--- back-end/models/animalImageModel.js
+++ back-end/models/animalImageModel.js
@@ -1,2 +1,40 @@
 const pool = require("../utils/db");
 
+exports.getAnimalImage1 = async (animalId) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT image1 FROM animal_images WHERE animal_id = $1', [animalId]);
+        client.release();
+
+        return result.rows[0]?.image1 || null;
+    } catch (err) {
+        console.error('Error executing query', err);
+        throw err;
+    }
+};
+
+exports.getAnimalImage2 = async (animalId) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT image2 FROM animal_images WHERE animal_id = $1', [animalId]);
+        client.release();
+
+        return result.rows[0]?.image2 || null;
+    } catch (err) {
+        console.error('Error executing query', err);
+        throw err;
+    }
+};
+
+exports.getAnimalImage1 = async (animalId) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT image3 FROM animal_images WHERE animal_id = $1', [animalId]);
+        client.release();
+
+        return result.rows[0]?.image3 || null;
+    } catch (err) {
+        console.error('Error executing query', err);
+        throw err;
+    }
+};
